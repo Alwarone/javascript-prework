5f5c5b2def5768d592b0393a8614ce1ec4697e58
Bartłomiej Dobrowolski <alwarone@gmail.com>
2020-06-11 17:33:25 +0200

    Add two blocks else if to script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
 let computerMove = `kamień`;
 
-function printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!'){
-	let div = document.createElement('div');
-	div.innerHTML = 'Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!';
-    document.getElementById('messages').appendChild(div);
-}
-
-let playerMove = `papier`;
-
-function printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!'){
-	let div = document.createElement('div');
-	div.innerHTML = 'Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!';
-    document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
