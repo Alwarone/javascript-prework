e6a41709eabc82d4b6babcec457791decbb4b3bc
Bartłomiej Dobrowolski <alwarone@gmail.com>
2020-06-11 16:44:09 +0200

    Add index.html & script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-function printMessage(msg){
+let computerMove = `kamień`;
+
+function printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!'){
 	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+	div.innerHTML = 'Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!';
+    document.getElementById('messages').appendChild(div);
+}
+
+let playerMove = `papier`;
+
+function printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!'){
+	let div = document.createElement('div');
+	div.innerHTML = 'Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!';
+    document.getElementById('messages').appendChild(div);
 }
 
 function clearMessages(){
